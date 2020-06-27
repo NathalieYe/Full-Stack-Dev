4386511a233e391a669ac1db51e10c7292274fa6
Nathalie Ye <nathalieymy@gmail.com>
2020-06-27 17:44:21 -0400

    submit ps6

---
+++ ps5b/configs/nytBooksAPI.js
@@ -0,0 +1,7 @@
+const nytbConfig = {
+    key: 'OlCXYuB2sp4Z5lWGZqcZBIvc0FxU73wq',
+    url: 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json'
+    // url: 'https://api.nytimes.com/svc/books/v3/reviews.json'
+}
+
+module.exports = nytbConfig;
---
+++ ps6/src/app/data/haruki-murakami.ts
@@ -0,0 +1,17 @@
+export class HARUKIBOOK{
+  title: string;
+  description: string;
+  contributor: string;
+  author: string;
+  contributor_note: string;
+  price: number;
+  age_group: string;
+  publisher: string;
+  isbns: object[];
+  ranks_history: object[];
+  reviews: object[];
+}
+// status: string;
+// copyright: string;
+// num_results: number;
+// results: object[];
